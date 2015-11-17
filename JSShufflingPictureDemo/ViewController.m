93c05b402976cf27e7d678f80ac19a30d6eaecd9
JhonSun <1193086345@qq.com>
2015-11-17 15:40:17 +0800

    使用n+2的方式实现无线循环

--- JSShufflingPictureDemo/ViewController.m
+++ JSShufflingPictureDemo/ViewController.m
@@ -18,6 +18,7 @@
 @property (nonatomic, strong) NSTimer *timer;
 
 @property (nonatomic, assign) NSInteger index;
+@property (nonatomic, assign) BOOL isFirst;
 
 @end
 
@@ -26,7 +27,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.pageControl.numberOfPages = self.imageArray.count;
+    self.pageControl.numberOfPages = self.imageArray.count - 2;
+    self.isFirst = YES;
     [self addTimer];
 }
 
@@ -38,7 +40,7 @@
 #pragma mark - get
 - (NSArray *)imageArray {
     if (!_imageArray) {
-        _imageArray = [NSArray arrayWithObjects:[UIImage imageNamed:@"banner1"], [UIImage imageNamed:@"banner2"], [UIImage imageNamed:@"banner3"], nil];
+        _imageArray = [NSArray arrayWithObjects:[UIImage imageNamed:@"banner3"], [UIImage imageNamed:@"banner1"], [UIImage imageNamed:@"banner2"], [UIImage imageNamed:@"banner3"], [UIImage imageNamed:@"banner1"], nil];
     }
     return _imageArray;
 }
@@ -64,18 +66,29 @@
 - (void)chagePageControlCurrentWithScrollView:(UIScrollView *)scrollView {
     CGFloat collectionViewWidth = self.collectionView.bounds.size.width;
     self.index = (scrollView.contentOffset.x - collectionViewWidth / 2) / collectionViewWidth + 1;
-    self.pageControl.currentPage = self.index % self.imageArray.count;
+//    NSUInteger page = self.index % self.imageArray.count;
+    
+    if (self.index == self.imageArray.count - 1) {
+        [self.collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForItem:1 inSection:0] atScrollPosition:UICollectionViewScrollPositionNone animated:NO];
+        self.index = 0;
+    } else if (self.index == 0) {
+        [self.collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForItem:self.imageArray.count - 2 inSection:0] atScrollPosition:UICollectionViewScrollPositionNone animated:NO];
+        self.index = self.imageArray.count - 2;
+    } else {
+        self.index -= 1;
+    }
+    self.pageControl.currentPage = self.index;
 }
 
 #pragma mark - UICollectionViewDataSource
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
-    return self.imageArray.count * 10000;
+    return self.imageArray.count;
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
     JSImageViewCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:imageViewCollectionViewCellIdentifier forIndexPath:indexPath];
-    NSUInteger page = indexPath.item % self.imageArray.count;
-    cell.image = [self.imageArray objectAtIndex:page];
+//    NSUInteger page = indexPath.item % self.imageArray.count;
+    cell.image = [self.imageArray objectAtIndex:indexPath.item];
     return cell;
 }
 
@@ -84,7 +97,14 @@
     return CGSizeMake([UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height);
 }
 
-#pragma mark - UIScrollViewDelegate
+#pragma mark - UICollectionViewDelegate
+- (void)collectionView:(UICollectionView *)collectionView willDisplayCell:(UICollectionViewCell *)cell forItemAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.item == 0 && _isFirst) {
+        [collectionView scrollToItemAtIndexPath:[NSIndexPath indexPathForItem:1 inSection:0] atScrollPosition:UICollectionViewScrollPositionNone animated:NO];
+        _isFirst = NO;
+    }
+}
+
 //开始拖动时，停止定时器
 - (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
     [self removeTimer];
